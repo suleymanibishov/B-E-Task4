884f4cc9151f3ff84f432ba31c4a7aa77dcdca43
suleymanibishov <suleyman22022@gmail.com>
2023-11-03 07:35:25 +0400

    Task

---
+++ task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetReverseStringName("Suleyman Semed Tural Mirqubad"));
+        }
+        static string GetReverseStringName(string str)
+        {
+            var strs = str.Split(' ');
+            string newStr = "";
+            foreach (var s in strs)
+            {
+                for (int i = s.Length-1; i >= 0; i--)
+                {
+                    newStr += s[i];
+                }
+                newStr += ' ';
+            }
+            return newStr;
+        }
+    }
+}
